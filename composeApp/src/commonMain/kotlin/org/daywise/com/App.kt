b1a8a5a3155c79955147d533fb3bc8fbc3d8ad98
Catalin Vanciu <catalin.vanciu@vspartners.us>
2025-07-28 15:23:07 +0300

    - add padding

--- composeApp/src/commonMain/kotlin/org/daywise/com/App.kt
+++ composeApp/src/commonMain/kotlin/org/daywise/com/App.kt
@@ -3,13 +3,16 @@ package org.daywise.com
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.task.businesslogicshared.platform
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -49,8 +52,9 @@ fun App() {
                         ),
                         contentDescription = "Lottie animation"
                     )
+                    Spacer(modifier = Modifier.padding(16.dp))
+                    WeatherScreen()
                 }
-                WeatherScreen()
             }
         }
     }
